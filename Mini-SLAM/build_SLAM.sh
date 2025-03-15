a67b1442423a437c284dca69fc5d641bc6b06788
Xavier Anadon <xvanadon@gmail.com>
2025-03-15 11:30:11 +0100

    First dbow2 integration

--- Mini-SLAM/build_SLAM.sh
+++ Mini-SLAM/build_SLAM.sh
@@ -4,3 +4,5 @@ cmake -B build
 cmake --build build --target mono_euroc
 cmake --build build --target mono_tumvi
 cmake --build build --target mono_tumrgbd
+cmake --build build --target dbow2_tests
+cmake --build build --target minislam_dbow2
